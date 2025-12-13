215bd856135fadd7b934970561e21a345c5bf22a
nusratnova5 <nusrat.nova16@gmail.com>
2025-12-14 00:53:44 +0600

    index.js modified

--- api/index.js
+++ api/index.js
@@ -1,19 +1,10 @@
-// export default function handler(req, res) {
-//   res.status(200).json({ 
-//     message: "Smart Trimmer Backend API",
-//     endpoints: {
-//       createOrder: "/api/create-order.js (POST)"
-//     }
-//   });
-// }
+export default function handler(req, res) {
+  res.status(200).json({ 
+    message: "Smart Trimmer Backend API",
+    endpoints: {
+      createOrder: "/api/create-order.js (POST)"
+    }
+  });
+}
 
 
-import express from "express";
-import createOrderRoute from "./create-order.js";
-
-const app = express();
-app.use(express.json());
-
-app.use("/create-order", createOrderRoute);
-
-app.listen(5000, () => console.log("Server running on port 5000"));
